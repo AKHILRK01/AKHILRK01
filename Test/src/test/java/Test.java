2a514a5313732aaf913728aa0718c440bdd51f6f
AKHILRK01 <48675374+AKHILRK01@users.noreply.github.com>
2022-05-18 18:02:05 +0530

    Add files via upload

--- Test/src/test/java/Test.java
+++ Test/src/test/java/Test.java
@@ -1,5 +1,7 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
@@ -14,12 +16,17 @@ public class Test {
         driver.manage().window().maximize();
 
         driver.get("https://en.wikipedia.org/wiki/Pushpa:_The_Rise");
+        String elementPresent = driver.findElement(By.xpath("//*[@id='firstHeading']/i")).getText();
+        Assert.assertEquals(elementPresent, "Pushpa: The Rise");
 
         String relDatewiki = driver.findElement(By.xpath("//tbody//tr[12]")).getText();
         String languagewiki = driver.findElement(By.xpath("//tbody//tr[14]")).getText();
 
 
         driver.get("https://www.imdb.com/title/tt9389998/");
+        String textPresent = driver.findElement(By.xpath("//h1[text()='Pushpa: The Rise - Part 1']")).getText();
+        Assert.assertEquals(textPresent, "Pushpa: The Rise - Part 1");
+
         String relDateimdb = driver.findElement(By.xpath("//a[text()='January 7, 2022 (United States)']")).getText();
         String languageimdb = driver.findElement(By.xpath("//a[text()='India']")).getText();
 
